a3eddbe7883d369dfc35821eadaf96c706c0e8d4
Rhys Frazier <rhysrfrazier@gmail.com>
2024-01-29 13:01:44 -0500

    added two custom fonts

--- nextjs-dashboard/app/ui/fonts.ts
+++ nextjs-dashboard/app/ui/fonts.ts
@@ -1,3 +1,9 @@
-import { Inter } from 'next/font/google'; // you can pull in any google font this way, and it will be downloaded at build time and hosted with other static assets. No additional network requests required
+import { Inter, Lusitana } from 'next/font/google'; // you can pull in any google font this way, and it will be downloaded at build time and hosted with other static assets. No additional network requests required
 
 export const inter =  Inter({subsets: ['latin'] });
+export const lusitana = Lusitana(
+    {
+        subsets: ['latin'],
+        weight: ['400', '700']
+    }
+)
